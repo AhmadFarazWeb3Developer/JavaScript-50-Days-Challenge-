547046e055acebfce165e5cb155805eceda4cdf4
Ahmad Faraz <129995311+AhmadFarazWeb3Developer@users.noreply.github.com>
2024-01-30 18:33:15 +0500

    Add files via upload

---
+++ Day2/3_numbers.js
@@ -0,0 +1,9 @@
+// ----- Numbers ------
+let amount = 400;
+console.log(amount.toFixed(2)); // Used in e-commerce applications for precision
+
+amount = 23.456123;
+console.log(amount.toPrecision(4)); // 23.45: converts value to a precise value
+
+const pkr = 10000000;
+console.log(pkr.toLocaleString()); // 10,000,000: converts to readable format
