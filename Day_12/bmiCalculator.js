492c8bac13e47cccd414d259a22d52b65ab31c5a
Ahmad Faraz <129995311+AhmadFarazWeb3Developer@users.noreply.github.com>
2024-02-09 20:54:11 +0500

    Update bmiCalculator.js

--- Day_12/bmiCalculator.js
+++ Day_12/bmiCalculator.js
@@ -1,131 +1,31 @@
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: sans-serif;
-}
 
-.container {
-    width: 50vh;
-    height: 90vh;
-    background-color: #e0e0e0;
-}
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+    // to prevent values from being sent to the server
+    e.preventDefault();
+    const height = parseFloat(document.querySelector("#height").value);
+    const weight = parseFloat(document.querySelector("#weight").value);
+    const results = document.querySelector("#results");
+    const details = document.querySelector("#details");
 
-body {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: black;
-}
-
-h1 {
-    font-size: 20px;
-    text-align: center;
-    margin-top: 20px;
-    color: rgb(183, 60, 15);
-}
-
-#inputs {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    position: relative;
-    margin-top: 25px;
-    padding-bottom: 20px;
-    background-color: rgb(223, 128, 94);
-    height: 180px;
-}
-
-#inputs input {
-    width: 200px;
-    border-radius: 4px;
-    border: 2px solid rgb(219, 100, 56);
-    outline-color: rgb(223, 128, 94);
-    height: 30px;
-    font-size: 12px;
-
-}
-
-#label1,
-#label2 {
-    margin-top: 20px;
-    font-size: 12px;
-    font-weight: 700;
-    color: #fdfdfd;
-}
-
-#height,
-#weight {
-    width: 150px;
-    height: 25px;
-    margin-top: 5px;
-    font-size: 16px;
-    text-align: center;
-}
-
-#weight-guide {
-    width: 22%;
-    text-align: center;
-    position: absolute;
-    bottom: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    margin-bottom: 59.5px;
-    height: 120px;
-    border-top-left-radius: 12px;
-    border-top-right-radius: 12px;
-    background-color: rgb(201, 191, 223);
-    color: #230f5c;
-}
-
-#weight-guide p {
-    margin-top: 5px;
-    font-size: 12px;
-}
-
-#weight-guide h3 {
-    font-size: 14px;
-    font-weight: 700;
-    margin-top: 20px;
-    margin-bottom: 10px;
-}
-
-#results,
-#details {
-    font-size: 20px;
-    text-align: center;
-    color: #3a9a48;
-    font-weight: bold;
-    color: #230f5c;
-}
-
-#results {
-    margin-top: 20px;
-}
-
-#details {
-    margin-top: 5px;
-    font-weight: lighter;
-}
-
-button {
-    width: 150px;
-    height: 30px;
-    letter-spacing: 1.5px;
-    margin-left: 70px;
-    margin-top: 25px;
-    background-color: #3a9a48;
-    border-radius: 4px;
-    color: #fdfdfd;
-    text-decoration: none;
-    border: none;
-    font-size: 16px;
-    text-align: center;
-    box-shadow: 4px 2px 5px rgba(39, 109, 74, 0.8);
-    cursor: pointer;
-}
-
-button:hover {
-    background-color: #347f40;
-}
+    if (height === "" || isNaN(height) || height <= 0) {
+        results.innerHTML = "Please provide a valid height";
+    } else if (height === "" || isNaN(weight) || weight <= 0) {
+        results.innerHTML = "Please provide a valid weight";
+    } else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        results.innerHTML = `<span>${bmi}</span>`;
+        // for underweight
+        if (bmi < 18.6) {
+            details.innerHTML = `<span>You are underweight</span>`;
+        }
+        // for normal weight
+        else if (bmi >= 18.6 && bmi <= 24.9) {
+            details.innerHTML = `<span>Your weight is Normal</span>`;
+        }
+        // for overweight
+        else {
+            details.innerHTML = `<span>You are overweight</span>`;
+        }
+    }
+});
