cdc6b0d09cc0335aa5ae464dc50d6dce5e4c485d
Ahmad Faraz <129995311+AhmadFarazWeb3Developer@users.noreply.github.com>
2024-02-17 15:56:32 +0500

    Create fetchAPI.js

---
+++ Day_20/fetchAPI.js
@@ -0,0 +1,14 @@
+Dconst url = "https://jsonplaceholder.typicode.com/posts";
+fetch(url)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log("Data:", data);
+  })
+  .catch((error) => {
+    console.error("Fetch error:", error);
+  });
