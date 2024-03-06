e16a14ceb718940880c8d68ac33e15d60875073d
Ahmad Faraz <129995311+AhmadFarazWeb3Developer@users.noreply.github.com>
2024-03-06 11:05:53 +0500

    Add files via upload

---
+++ Day_38/notesApp.js
@@ -0,0 +1,43 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+const notes = document.querySelectorAll(".input-box");
+
+const showNotes = () => {
+  notesContainer.innerHTML = localStorage.getItem("notes");
+};
+
+showNotes();
+
+const updateStorage = () => {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+};
+
+createBtn.addEventListener("click", () => {
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.src = "images/delete.png";
+  notesContainer.appendChild(inputBox).append(img);
+  updateStorage();
+});
+
+notesContainer.addEventListener("click", (e) => {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+    updateStorage();
+  } else if (e.target.tagName === "P") {
+    notes.forEach((note) => {
+      note.onkeyup = () => {
+        updateStorage();
+      };
+    });
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    document.execCommand("insertLineBreak");
+    event.preventDefault();
+  }
+});
